5dc815eefc27a5e0f9b9653951657a10094bd81f
farseen33 <farseen33m@gmail.com>
2024-12-08 09:39:28 +0530

    Initial commit - React frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navbar from './components/common/Navbar';
+import Sidebar from './components/common/Sidebar';
+import Products from './components/products/Products';
+import Inventory from './components/inventory/Inventory';
+import { Box } from '@mui/material';
+import StockMovement from './components/inventory/StockMovement';
+import Dashboard from './components/Dashboard';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Box sx={{ display: 'flex' }}>
+          <Navbar />
+          <Sidebar />
+          <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8, ml: { sm: 30 } }}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/inventory" element={<Inventory />} />
+              <Route path="/sales" element={<></>} />
+              <Route path="/stock-movement" element={<StockMovement />} />
+            </Routes>
+          </Box>
+        </Box>
+      </Router>
+
+
+    </ThemeProvider>
   );
 }
 
